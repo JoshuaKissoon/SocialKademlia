091fe53ced6c919708ac35d34db73dbb806e57b4
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-05-01 15:04:07 +0530

    Routing Table - Added a routing table interface to code to Interface not implementation
    Social Kad Routing Table
    - Now I added a Social Kad routing table interface and implementation
    - Left the Base Kademlia routing table implementation in tact to foster code reuse

--- src/kademlia/routing/SocialKadRoutingTable.java
+++ src/kademlia/routing/SocialKadRoutingTable.java
@@ -3,26 +3,26 @@ package kademlia.routing;
 import java.util.List;
 
 /**
- * Extensions on a Bucket to make it work for Social Kademlia.
+ * Extensions on a RoutingTable to make it work for Social Kademlia.
  * A connection is directly related to the Distributed Online Social Network(DOSN);
  * Every Actor(user) on the DOSN have connections (friends);
- * this extensions provide mechanisms for the bucket to keep track of connections.
+ * this extensions provide mechanisms for the routing table to keep track of connections.
  *
  * @author Joshua Kissoon
  * @created 20140501
  */
-public interface SociakKadBucket
+public interface SocialKadRoutingTable extends KadRoutingTable
 {
 
     /**
-     * Adds a Connection to the bucket
+     * Adds a Connection to the routing table
      *
      * @param c the new connection
      */
     public void insertConnection(Connection c);
 
     /**
-     * Checks if this bucket contain a connection.
+     * Checks if this routing table contain a connection.
      *
      * @param c The connection to check for
      *
@@ -31,7 +31,7 @@ public interface SociakKadBucket
     public boolean containsConnection(Connection c);
 
     /**
-     * Remove a connection from this bucket.
+     * Remove a connection from this routing table.
      *
      * @param c The connection to remove
      *
@@ -40,7 +40,7 @@ public interface SociakKadBucket
     public boolean removeConnection(Connection c);
 
     /**
-     * @return An Iterable structure with all connections in this bucket
+     * @return An Iterable structure with all connections in this routing table
      */
     public List<Connection> getConnections();
 }
